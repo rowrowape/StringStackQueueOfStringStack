f34821f65e2d31312931f2092bb6991898cb5d9f
AweMath <dart.fk712@gmail.com>
2015-12-18 02:24:55 +0600

    add enter after 9th item

--- main.cpp
+++ main.cpp
@@ -13,84 +13,89 @@ int main() {
     StringStackQueue queue;
     string value;
     int key;
-    while (key!=10) {
+    while (key != 10) {
         cout << "1. Create stack" << endl << "2. Enter higher element to stack" << endl <<
         "3. Delete higher element from stack" << endl;
         cout << "5. Clear stack" << endl << "6.Delete stack" << endl;
-        cout << "7. Push stack to queue" << "8. Take and delete stack from queue" << endl << "9. Clear queue";
-        cout << "10. Exit";
+        cout << "7. Push stack to queue" << "8. Take and delete stack from queue" << endl << "9. Clear queue" << endl;
+        cout << "10. Exit" << endl;
         cin >> key;
-        switch (key) {
-            case 1 : {
-                if (a != 0) {
-                    cout << "You allready have active stack";
+        try {
+            switch (key) {
+                case 1 : {
+                    if (a != 0) {
+                        cout << "You allready have active stack";
+                        break;
+                    }
+                    a = new StringStack();
                     break;
                 }
-                a = new StringStack();
-                break;
-            }
-            case 2 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 2 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    cin >> value;
+                    putStringStackElem(*a, value);
                     break;
                 }
-                cin >> value;
-                putStringStackElem(*a, value);
-                break;
-            }
-            case 3 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 3 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    deleteStringStackElem(*a);
                     break;
                 }
-                deleteStringStackElem(*a);
-                break;
-            }
-            case 4 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 4 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    cout << takeStringStackElem(*a);
                     break;
                 }
-                cout << takeStringStackElem(*a);
-                break;
-            }
-            case 5 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 5 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    clear(*a);
                     break;
                 }
-                clear(*a);
-                break;
-            }
-            case 6 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 6 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    clear(*a);
+                    delete a;
                     break;
                 }
-                clear(*a);
-                delete a;
-                break;
-            }
-            case 7 : {
-                if (a == 0) {
-                    cout << "You haven`t active stack";
+                case 7 : {
+                    if (a == 0) {
+                        cout << "You haven`t active stack";
+                        break;
+                    }
+                    push(queue, *a);
                     break;
                 }
-                push(queue, *a);
-                break;
-            }
-            case 8 : {
-                if (a != 0) {
-                    cout << "You allready have active stack";
+                case 8 : {
+                    if (a != 0) {
+                        cout << "You allready have active stack";
+                        break;
+                    }
+                    a = poll(queue);
                     break;
                 }
-                a = poll(queue);
-                break;
+                case 9 : {
+                    clear(queue);
+                    break;
+                }
             }
-            case 9 : {
-                clear(queue);
-                break;
-            }
+        } catch (string s) {
+            cout << s;
+            cin.get();
         }
     }
     return 0;
