3bdc7ab4ab8ffcffc375233df53956b6a4f3e689
rowrowape <zewwws13@gmail.com>
2015-12-17 22:57:04 +0300

    add put function

--- StringStack.cpp
+++ StringStack.cpp
@@ -2,8 +2,27 @@
 #include <string>
 using namespace std;
 
+struct StringStack{
+StringStackElem *first;
+    StringStack(){
+        first = 0;
+    }
+};
 struct StringStackElem{
-    StringStack *next;
+    StringStackElem *next;
     string value;
 };
 
+
+
+void putStringStackElem(StringStack &a, string &value){
+    if (a.first = 0)
+    {
+        a.first->next = 0;
+        a.first->value = value;
+        return;
+    }
+StringStackElem newElem;
+newElem.next = first;
+first = newElem;
+}
--- StringStack.h
+++ StringStack.h
@@ -1,13 +1,9 @@
-//
-// Created by awemath on 17.12.15.
-//
-
 #ifndef STRINGSTACKQUEUEOFSTRINGSTACK_STRINGSTACK_H
 #define STRINGSTACKQUEUEOFSTRINGSTACK_STRINGSTACK_H
 
 #include <string.h>
 struct StringStackElem;
-void putStringStackElem(*current);
+void putStringStackElem(StringStack &a, string &value);
 void takeStringStackElem(*current);
 void deleteStringStackElem(*current);
 
