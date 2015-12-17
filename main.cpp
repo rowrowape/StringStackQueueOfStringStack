5c2ac09bc18d4498184833301f3bdee134857bfe
rowrowape <zewwws13@gmail.com>
2015-12-18 00:08:59 +0300

    fix build errors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include "StringStack.h"
+#include "StringStack.cpp"
+#include <string>
 using namespace std;
 
 int main() {
+    StringStack SS = new StringStack;
+    put(SS, "kli");
+    put (SS, "8");
+    pollStringStackElem(SS);
+    clear(SS);
+
+
     return 0;
 }
